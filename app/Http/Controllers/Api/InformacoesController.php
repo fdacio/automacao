695a31eec950c6efcf80b892f700b446b91ee55b
Dacio <fdacio@gmail.com>
2021-06-16 02:22:26 -0300

    CRUD informacoes

--- app/Http/Controllers/Api/InformacoesController.php
+++ app/Http/Controllers/Api/InformacoesController.php
@@ -25,9 +25,9 @@ class InformacoesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show()
+    public function show($id)
     {
-        $informacao = Informacao::get()->last();
+        $informacao = Informacao::find($id);
         return $informacao->texto;
     }
 
