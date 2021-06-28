47a239cc024169a24646d337b6e26519ba1f1fbd
Dacio <fdacio@gmail.com>
2021-06-28 00:21:55 -0300

    Delete componente

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('componentes', 'ComponentesController');
+Route::get('componentes/{componente}', 'ComponentesController@destroy')->name('componentes.destroy');
 Route::resource('informacoes', 'InformacoesController')->parameters(['informacoes' => 'informacao']);
 Route::get('infomacoes/destroy/{informacao}', 'InformacoesController@destroy')->name('informacoes.destroy');
