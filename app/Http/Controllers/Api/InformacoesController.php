b7257c4bf483cd4587b5629cc208f3674c54b61e
Dacio <fdacio@gmail.com>
2021-07-25 01:02:40 -0300

    fuzo

--- app/Http/Controllers/Api/InformacoesController.php
+++ app/Http/Controllers/Api/InformacoesController.php
@@ -53,6 +53,9 @@ class InformacoesController extends Controller
         $time = \Carbon\Carbon::now($timeZone['timezone'])->format('H:i:s'); 
         
         $index++;
+        if($index > 5) {
+            $index = 1;
+        }
         return ['date-time' => ['local' => $city, 'date' => $date, 'time' => $time]];
     }
 }
