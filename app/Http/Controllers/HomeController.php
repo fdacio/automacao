a881c1e18d98fe72a442cf010db49b425e019fa9
Dacio <fdacio@gmail.com>
2021-09-12 15:56:05 -0300

    iot

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
     public function index()
     {
         
-        $componentes = Componente::get();
+        $componentes = Componente::whereNotIn('nome', ['IoT 0', 'IoT 2'])->get();
         return view('home', compact('componentes'));
     }
 }
