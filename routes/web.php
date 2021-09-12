b918a0b1934def9254ab59ce2cc2202d6349bfa3
Dacio <fdacio@gmail.com>
2021-09-12 12:50:33 -0300

    iot

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::resource('componentes', 'ComponentesController');
 Route::get('componentes/{componente}', 'ComponentesController@destroy')->name('componentes.destroy');
 Route::resource('informacoes', 'InformacoesController')->parameters(['informacoes' => 'informacao']);
 Route::get('infomacoes/destroy/{informacao}', 'InformacoesController@destroy')->name('informacoes.destroy');
+Route::get('iot', 'IotController@index')->name('iot.index');
