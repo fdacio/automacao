3eb607b5a73a1eb6f5fba56259aa75bc3ddd95cf
Dácio Braga <dacio@moderar.com.br>
2021-09-22 10:07:27 -0300

    show modal presenca

--- resources/views/iot/index.blade.php
+++ resources/views/iot/index.blade.php
@@ -6,8 +6,7 @@
         <div class="row">
             @foreach ($componentes as $componente)
                 <div class="col-md-4 text-center">
-                    <div class="btn btn-power" data-id="{{ $componente->id }}"
-                        data-clicked="false">
+                    <div class="btn btn-power" data-id="{{ $componente->id }}" data-clicked="false">
                         <div class="btn-rect">
                             <div class="fa-ico">
                                 <i class="fa fa-power-off"></i>
@@ -19,10 +18,10 @@
                         </div>
                         <h3>{{ $componente->nome }}</h3>
                     </div>
-                </div>                
+                </div>
             @endforeach
             <div class="col-md-4 text-center">
-                <div class="btn btn-sensor">
+                <div class="btn btn-sensor" data-toggle="modal" data-target="#presencaModal">
                     <div class="btn-rect">
                         <div class="fa-ico">
                             <i class="fa fa-street-view"></i>
@@ -32,11 +31,30 @@
                         </div>
                         <div class="clearfix"></div>
                     </div>
-                    <h3>Aproximação</h3>
+                    <h3>Sensor de Presença</h3>
                 </div>
             </div>
         </div>
         <hr class="featurette-divider">
+    </div>
+
+    <!-- Presenca Modal -->
+    <div class="modal fade" id="presencaModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Sensor de Presença</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="border text-center">
+                        <i class="fa fa-spinner fa-spin mt-4" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
@@ -45,31 +63,31 @@
         $(document).ready(function() {
 
             var updateComponente = function(componente) {
-                    var id = componente.attr('data-id');
-                    var icon = componente.find('.btn-rect .fa-ico');
-                    var url = "{{ route('api.componente.show', '_id_') }}".replace('_id_', id);
-                    $.get(url, function() {
-                        icon.html('<i class="fa fa-spin fa-spinner"></i>');
-                    }).done(function(data) {
-                        if (data) {
-                            var sinal = (data.sinal == 1) ? true : false;
-                            $(componente).attr('data-clicked', sinal);
-                            if (sinal) {
-                                $(componente).find('.text-on-off').html('ON').removeClass('badge-danger')
-                                    .addClass('badge-success');
-                            } else {
-                                $(componente).find('.text-on-off').html('OFF').removeClass(
-                                    'badge-success').addClass('badge-danger');
-                            }
-                            icon.html('<i class="fa fa-power-off"></i>');
-                            icon.css('color', data.cor);
-                        } 
-                    });
+                var id = componente.attr('data-id');
+                var icon = componente.find('.btn-rect .fa-ico');
+                var url = "{{ route('api.componente.show', '_id_') }}".replace('_id_', id);
+                $.get(url, function() {
+                    icon.html('<i class="fa fa-spin fa-spinner"></i>');
+                }).done(function(data) {
+                    if (data) {
+                        var sinal = (data.sinal == 1) ? true : false;
+                        $(componente).attr('data-clicked', sinal);
+                        if (sinal) {
+                            $(componente).find('.text-on-off').html('ON').removeClass('badge-danger')
+                                .addClass('badge-success');
+                        } else {
+                            $(componente).find('.text-on-off').html('OFF').removeClass(
+                                'badge-success').addClass('badge-danger');
+                        }
+                        icon.html('<i class="fa fa-power-off"></i>');
+                        icon.css('color', data.cor);
+                    }
+                });
             }
 
             var loadComponente = function() {
                 var componentes = $(".btn-power");
-                $.each(componentes, function(key, componente) {                    
+                $.each(componentes, function(key, componente) {
                     updateComponente($(componente));
                 });
             }
@@ -99,31 +117,36 @@
                     type: 'PUT',
                     success: function(response) {
                         if (response.success) {
-                           updateComponente(button);     
-                           _execute = true;
+                            updateComponente(button);
+                            _execute = true;
                         }
                     }
                 });
             });
 
+            /*
+             * Sensor de Presença
+             */
             setInterval(function() {
                 $.get("{{ route('api.presenca.show') }}", function(dados) {
                     var presenca = dados.presenca;
                     if (dados.presenca != undefined) {
                         if (dados.presenca) {
-                            $('.valor-leitura').addClass('badge-danger').removeClass('badge-success');
+                            $('.valor-leitura').addClass('badge-danger').removeClass(
+                                'badge-success');
                             $('.valor-leitura').html("Presença de Movimento");
                         } else {
-                            $('.valor-leitura').addClass('badge-success').removeClass('badge-danger');
+                            $('.valor-leitura').addClass('badge-success').removeClass(
+                                'badge-danger');
                             $('.valor-leitura').html("Não há movimento");
                         }
-                       
                     }
                 });
             }, 2000);
 
+            
+
 
         }); // fim documento jquery
-
     </script>
 @endsection
