f14e6bdcf2745a5edf770732898a438b70587882
Dácio Braga <dacio@moderar.com.br>
2021-09-22 17:59:31 -0300

    lista de presenças

--- resources/views/iot/index.blade.php
+++ resources/views/iot/index.blade.php
@@ -154,8 +154,8 @@
                     var li = $('<li class="list-group-item">');
                     var dataHora = item.created_at;
                     var row = '<div class="row">' +
-                        '<div class="col-md-2 col-sm-2 text-right">' + acao + '</div>' +
-                        '<div class="col-md-10 col-sm-10">' + dataHora.substring(11) + '</div>' +
+                        '<div class="col-md-3 col-sm-3 text-right">' + acao + '</div>' +
+                        '<div class="col-md-9 col-sm-9">' + dataHora.substring(11) + '</div>' +
                         '</div>';
                     li.html(row);
                     lista.append(li);
