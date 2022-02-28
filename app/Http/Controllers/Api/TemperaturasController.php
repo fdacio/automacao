1c954e7ac15947b638e7907c6177b868d1280d42
Dacio <fdacio@gmail.com>
2022-02-28 19:48:39 -0300

    correcao

--- app/Http/Controllers/Api/TemperaturasController.php
+++ app/Http/Controllers/Api/TemperaturasController.php
@@ -46,7 +46,7 @@ class TemperaturasController extends Controller
         $h_hr_max = 0;
         $h_min = 0;
         $h_hr_min = 0;
-
+        dd($temperaturas);
         if (!empty($temperaturas)) {
             $tempMax = $temperaturas->last();
             $tempMin = $temperaturas->first();
