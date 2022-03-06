fea2a5d152de6e0dd20cf5fbb696bb92c52cdc42
Dacio <fdacio@gmail.com>
2022-03-06 03:18:00 -0300

    ajuste

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,10 @@
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                     <li class="nav-item">
-                        <a class="nav-link text-light" aria-current="page" href="{{ route('home') }}">Painel de Controle</a>
+                        <a class="nav-link text-light" aria-current="page" href="{{ route('home') }}">Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" aria-current="page" href="{{ route('painel-controle.index') }}">Painel de Controle</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-light" aria-current="page"
