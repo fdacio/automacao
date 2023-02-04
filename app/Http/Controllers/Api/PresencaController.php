550cfaca0b229f1124f3cf61f3c3b437bab24128
Dacio <fdacio@gmail.com>
2023-02-04 00:48:57 -0300

    ajuste

--- app/Http/Controllers/Api/PresencaController.php
+++ app/Http/Controllers/Api/PresencaController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Automacao\Http\Controllers\Controller;
 use Automacao\Models\Presenca;
 use Carbon\Carbon;
+use Exception;
 
 class PresencaController extends Controller
 {
@@ -24,9 +25,13 @@ class PresencaController extends Controller
 
     public function show()
     {
-        $data1 = Carbon::now();
-        $presenca = Presenca::whereBetween('created_at', [$data1->format('Y-m-d'), $data1->addDays(1)->format('Y-m-d')])->orderBy('id', 'desc')->get()->first();
-        return $presenca;
+        try {
+            $data1 = Carbon::now();
+            $presenca = Presenca::whereBetween('created_at', [$data1->format('Y-m-d'), $data1->addDays(1)->format('Y-m-d')])->orderBy('id', 'desc')->get()->first();
+            return $presenca;
+        } catch(Exception $e) {
+            return response($e->getMessage(), 403);
+        }
     }
 
     public function index()
